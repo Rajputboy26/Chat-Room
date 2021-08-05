7f7040001cd0368e86cd44a97a34dd98f266cfb2
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-08-05 17:28:42 +0530

    added usemediaquery

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,18 +1,19 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 import DashBoard from './Dashboard';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  const is992px = useMediaQuery('(max-width: 992px)');
 
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={is992px} show={isOpen} onHide={close} placement="left">
         <DashBoard />
       </Drawer>
     </>
