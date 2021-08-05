a43b55503378176d5b667f5b6a48267ca4de8105
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-08-05 22:52:58 +0530

    added editable nickname

--- src/components/dashboard/Slider.js
+++ src/components/dashboard/Slider.js
@@ -1,10 +1,15 @@
 // ye video k hissab se index.js h
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 const DashBoard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
+  const onSave = async newNickName => {
+    console.log(newNickName);
+  };
   return (
     <>
       <Drawer.Header>
@@ -12,6 +17,13 @@ const DashBoard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hello,{profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">NickName</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
