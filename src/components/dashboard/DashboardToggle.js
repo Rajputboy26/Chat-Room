5c547692708541eb5217dc1f52801275b6e516ec
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-08-10 02:01:00 +0530

    add real-time online/offline service

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react';
 import { Alert, Button, Drawer, Icon } from 'rsuite';
+import { isOfflineForDatabase } from '../../context/profile.context';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 import DashBoard from './Slider';
 
 const DashboardToggle = () => {
@@ -9,10 +10,17 @@ const DashboardToggle = () => {
   const is992px = useMediaQuery('(max-width: 992px)');
 
   const onSignOut = useCallback(() => {
-    auth.signOut();
-
-    Alert.info('Signed Out', 4000);
-    close();
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed Out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
 
   return (
