33c614048d3fd2ab8d5e737b6bd6181237b9f37d
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-08-09 17:46:41 +0530

    created room description modal

---
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -0,0 +1,36 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useModalState } from '../../../misc/custom-hooks';
+
+const RoomInfoBtnModal = () => {
+  const { isOpen, open, close } = useModalState();
+
+  const description = useCurrentRoom(d => d.description);
+  const name = useCurrentRoom(n => n.name);
+
+  return (
+    <>
+      <Button appearance="link" className="px-0" onClick={open}>
+        Room information
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>About {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6 className="mb-1">Description:</h6>
+          <p>{description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
