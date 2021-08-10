51ee00c2a029392bcffa61535e1b93b499a6c0c7
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-08-10 15:08:23 +0530

    button to give or remove admin permission

--- src/components/chat-window/message/MessageItem.js
+++ src/components/chat-window/message/MessageItem.js
@@ -1,16 +1,25 @@
-/* eslint-disable */
-import React from 'react';
+import React, { memo } from 'react';
 import TimeAgo from 'timeago-react';
-import PresenceDot from '../../PresenceDot';
+import { Button } from 'rsuite';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 
-const MessageItem = ({ message }) => {
+const MessageItem = ({ message, handleAdmin }) => {
   const { Author, createdAt, text } = message;
+
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(Author.uid);
+  const isAuthor = auth.currentUser.uid === Author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
     <li className="padded mb-1">
-      <div className="d-flex align-item-center font-bolder mb-1">
+      <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={Author.uid} />
         <ProfileAvatar
           src={Author.avatar}
@@ -22,17 +31,25 @@ const MessageItem = ({ message }) => {
           profile={Author}
           appearance="link"
           className="p-0 ml-1 text-black"
-        />
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(Author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? 'Remove admin permission'
+                : 'Give Admin permission'}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
         />
       </div>
       <div>
-        <span className="word-break-all">{text}</span>
+        <span className="word-breal-all">{text}</span>
       </div>
     </li>
   );
 };
 
-export default MessageItem;
+export default memo(MessageItem);
