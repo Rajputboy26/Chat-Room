66650666c7b6598abf34175aba826b486a00acfc
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-08-10 16:59:20 +0530

    added hover

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useCallback, useEffect, useState, useRef } from 'react';
 import { database } from './firebase';
 
 export function useModalState(defaultValue = false) {
@@ -46,4 +47,27 @@ export function usePresence(uid) {
     };
   }, [uid]);
   return presence;
+}
+
+export function useHover() {
+  const [value, setValue] = useState(false);
+
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener('mouseover', handleMouseOver);
+      node.addEventListener('mouseout', handleMouseOut);
+    }
+    return () => {
+      node.removeEventListener('mouseover', handleMouseOver);
+      node.removeEventListener('mouseout', handleMouseOut);
+    };
+  }, [ref.current]);
+
+  return [ref, value];
 }
